57f36ec319893abeb91305dd24d4850c03f6fbd4
Mark Lim <hello@milkram.com>
2017-06-12 01:16:10 -0400

    Submission for Assignment 2

--- assignment_2/assignment2.js
+++ assignment_2/assignment2.js
@@ -3,6 +3,10 @@ Exercise #1
 Create a for loop that prints out the numbers 1 to 100 in the console.
 */
 
+console.log("Exercise #1:")
+for (i=1; i<101; i++){
+    console.log(i);
+}
 
 
 /*
@@ -17,3 +21,29 @@ Write a loop that makes seven calls to console.log to output the following trian
 ######
 #######
 */
+
+console.log("Exercise #2:");
+for (i=1; i<8; i++){
+
+    // Using .length method, as per the hint provided
+    var string1 = '';
+    while (string1.length < i){
+        string1 = string1 + '#';
+    }
+    console.log(string1);
+
+    // Using .repeat method (googled)
+    /*
+    var string2 = '#';
+    console.log(string2.repeat(i));
+    */
+
+    // For loop within a for loop
+    /*
+    var string3 = '';
+    for (x=i; x>0; x--){
+        string3 = string3 + '#';
+    }
+    console.log(string3);
+    */
+}
